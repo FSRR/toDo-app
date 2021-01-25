84b548c73dabf54579cad33fbd6ec6a53cdfd4fe
FSRR <sebastianretizmail.com>
2021-01-24 20:19:16 -0500

    Estilos despues del backend

--- app.py
+++ app.py
@@ -66,8 +66,13 @@ def register():
         user.email = request.form['userEmail']
         user.password = request.form['userPass']
 
-        controller = UserController()
-        data = controller.registerUser(user)
+
+        if not (user.name and user.email and user.password):
+            print('si')
+            controller = UserController()
+            data = controller.registerUser(user)
+            alert = 'Rellena todos los campos'
+            return redirect(url_for('signin', alert = alert))
         if data:
             session['data'] = data
             return redirect(url_for('index'))
--- static/js/theme.js
+++ static/js/theme.js
@@ -1,17 +1,17 @@
 const body = document.getElementById('body')
-const headerBackground = document.getElementById('header-background')
+const headerBackground = document.getElementById('banner-background')
 const themeMode = document.getElementById('theme-mode')
 
 if(themeMode) {
     themeMode.addEventListener('click', () => {
         if(body.getAttribute('class') == 'dark') {
             body.classList.replace('dark', 'light')
-            headerBackground.classList.replace('header-dark', 'header-light')
+            headerBackground.classList.replace('banner-dark', 'banner-light')
             themeMode.setAttribute('src', '/static/img/icon-moon.svg')
             themeMode.setAttribute('alt', 'Dark Theme')
         } else {
             body.classList.replace('light', 'dark')
-            headerBackground.classList.replace('header-light', 'header-dark')
+            headerBackground.classList.replace('banner-light', 'banner-dark')
             themeMode.setAttribute('src', '/static/img/icon-sun.svg')
             themeMode.setAttribute('alt', 'Light Theme')
         }
